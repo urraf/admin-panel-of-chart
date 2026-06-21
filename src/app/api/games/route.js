503ad6948bc098f39cbb782aa2775dd4fb65a0b8
thenahraf <farhan.ug23@nsut.ac.in>
2026-06-22 01:31:41 +0530

    Fix production caching bug for APIs by adding force-dynamic

--- src/app/api/games/route.js
+++ src/app/api/games/route.js
@@ -1,5 +1,6 @@
 import { NextResponse } from 'next/server';
 import prisma from '@/lib/prisma';
+export const dynamic = 'force-dynamic';
 import { getServerSession } from 'next-auth';
 
 export async function GET() {
